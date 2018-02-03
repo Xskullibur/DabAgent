fbabb43618bd6cd58c3d6d7c692b9cac157ea607
Keegan Yip <dragonaccyip@yahoo.com.sg>
2018-02-03 14:17:15 +0800

    CustEdit Update 1

--- DabAgent/CustEdit.Designer.cs
+++ DabAgent/CustEdit.Designer.cs
@@ -29,10 +29,207 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label nRICLabel;
+            System.Windows.Forms.Label nameLabel;
+            System.Windows.Forms.Label contactLabel;
+            System.Windows.Forms.Label addressLabel;
+            System.Windows.Forms.Label date_of_BirthLabel;
+            System.Windows.Forms.Label countryLabel;
+            this.dabAgencyDataSet = new DabAgent.DabAgencyDataSet();
+            this.customerTBBS = new System.Windows.Forms.BindingSource(this.components);
+            this.customerTA = new DabAgent.DabAgencyDataSetTableAdapters.CustomerTBTableAdapter();
+            this.tam = new DabAgent.DabAgencyDataSetTableAdapters.TableAdapterManager();
+            this.nRICTextBox = new System.Windows.Forms.TextBox();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.contactTextBox = new System.Windows.Forms.TextBox();
+            this.addressTextBox = new System.Windows.Forms.TextBox();
+            this.date_of_BirthDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.countryTextBox = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            nRICLabel = new System.Windows.Forms.Label();
+            nameLabel = new System.Windows.Forms.Label();
+            contactLabel = new System.Windows.Forms.Label();
+            addressLabel = new System.Windows.Forms.Label();
+            date_of_BirthLabel = new System.Windows.Forms.Label();
+            countryLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dabAgencyDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customerTBBS)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dabAgencyDataSet
+            // 
+            this.dabAgencyDataSet.DataSetName = "DabAgencyDataSet";
+            this.dabAgencyDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // customerTBBS
+            // 
+            this.customerTBBS.DataMember = "CustomerTB";
+            this.customerTBBS.DataSource = this.dabAgencyDataSet;
+            // 
+            // customerTA
+            // 
+            this.customerTA.ClearBeforeFill = true;
+            // 
+            // tam
+            // 
+            this.tam.BackupDataSetBeforeUpdate = false;
+            this.tam.BookingTBTableAdapter = null;
+            this.tam.CustomerTBTableAdapter = this.customerTA;
+            this.tam.HolidayTBTableAdapter = null;
+            this.tam.UpdateOrder = DabAgent.DabAgencyDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // nRICLabel
+            // 
+            nRICLabel.AutoSize = true;
+            nRICLabel.Location = new System.Drawing.Point(54, 38);
+            nRICLabel.Name = "nRICLabel";
+            nRICLabel.Size = new System.Drawing.Size(36, 13);
+            nRICLabel.TabIndex = 1;
+            nRICLabel.Text = "NRIC:";
+            // 
+            // nRICTextBox
+            // 
+            this.nRICTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.customerTBBS, "NRIC", true));
+            this.nRICTextBox.Location = new System.Drawing.Point(129, 35);
+            this.nRICTextBox.Name = "nRICTextBox";
+            this.nRICTextBox.Size = new System.Drawing.Size(200, 20);
+            this.nRICTextBox.TabIndex = 2;
+            // 
+            // nameLabel
+            // 
+            nameLabel.AutoSize = true;
+            nameLabel.Location = new System.Drawing.Point(54, 64);
+            nameLabel.Name = "nameLabel";
+            nameLabel.Size = new System.Drawing.Size(38, 13);
+            nameLabel.TabIndex = 3;
+            nameLabel.Text = "Name:";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.customerTBBS, "Name", true));
+            this.nameTextBox.Location = new System.Drawing.Point(129, 61);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(200, 20);
+            this.nameTextBox.TabIndex = 4;
+            // 
+            // contactLabel
+            // 
+            contactLabel.AutoSize = true;
+            contactLabel.Location = new System.Drawing.Point(54, 90);
+            contactLabel.Name = "contactLabel";
+            contactLabel.Size = new System.Drawing.Size(47, 13);
+            contactLabel.TabIndex = 5;
+            contactLabel.Text = "Contact:";
+            // 
+            // contactTextBox
+            // 
+            this.contactTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.customerTBBS, "Contact", true));
+            this.contactTextBox.Location = new System.Drawing.Point(129, 87);
+            this.contactTextBox.Name = "contactTextBox";
+            this.contactTextBox.Size = new System.Drawing.Size(200, 20);
+            this.contactTextBox.TabIndex = 6;
+            // 
+            // addressLabel
+            // 
+            addressLabel.AutoSize = true;
+            addressLabel.Location = new System.Drawing.Point(54, 116);
+            addressLabel.Name = "addressLabel";
+            addressLabel.Size = new System.Drawing.Size(48, 13);
+            addressLabel.TabIndex = 7;
+            addressLabel.Text = "Address:";
+            // 
+            // addressTextBox
+            // 
+            this.addressTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.customerTBBS, "Address", true));
+            this.addressTextBox.Location = new System.Drawing.Point(129, 113);
+            this.addressTextBox.Name = "addressTextBox";
+            this.addressTextBox.Size = new System.Drawing.Size(200, 20);
+            this.addressTextBox.TabIndex = 8;
+            // 
+            // date_of_BirthLabel
+            // 
+            date_of_BirthLabel.AutoSize = true;
+            date_of_BirthLabel.Location = new System.Drawing.Point(54, 143);
+            date_of_BirthLabel.Name = "date_of_BirthLabel";
+            date_of_BirthLabel.Size = new System.Drawing.Size(69, 13);
+            date_of_BirthLabel.TabIndex = 9;
+            date_of_BirthLabel.Text = "Date of Birth:";
+            // 
+            // date_of_BirthDateTimePicker
+            // 
+            this.date_of_BirthDateTimePicker.DataBindings.Add(new System.Windows.Forms.Binding("Value", this.customerTBBS, "Date of Birth", true));
+            this.date_of_BirthDateTimePicker.Location = new System.Drawing.Point(129, 139);
+            this.date_of_BirthDateTimePicker.Name = "date_of_BirthDateTimePicker";
+            this.date_of_BirthDateTimePicker.Size = new System.Drawing.Size(200, 20);
+            this.date_of_BirthDateTimePicker.TabIndex = 10;
+            // 
+            // countryLabel
+            // 
+            countryLabel.AutoSize = true;
+            countryLabel.Location = new System.Drawing.Point(54, 168);
+            countryLabel.Name = "countryLabel";
+            countryLabel.Size = new System.Drawing.Size(46, 13);
+            countryLabel.TabIndex = 11;
+            countryLabel.Text = "Country:";
+            // 
+            // countryTextBox
+            // 
+            this.countryTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.customerTBBS, "Country", true));
+            this.countryTextBox.Location = new System.Drawing.Point(129, 165);
+            this.countryTextBox.Name = "countryTextBox";
+            this.countryTextBox.Size = new System.Drawing.Size(200, 20);
+            this.countryTextBox.TabIndex = 12;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(144, 234);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(103, 23);
+            this.btnSave.TabIndex = 13;
+            this.btnSave.Text = "Save Changes";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // CustEdit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(411, 331);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(nRICLabel);
+            this.Controls.Add(this.nRICTextBox);
+            this.Controls.Add(nameLabel);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(contactLabel);
+            this.Controls.Add(this.contactTextBox);
+            this.Controls.Add(addressLabel);
+            this.Controls.Add(this.addressTextBox);
+            this.Controls.Add(date_of_BirthLabel);
+            this.Controls.Add(this.date_of_BirthDateTimePicker);
+            this.Controls.Add(countryLabel);
+            this.Controls.Add(this.countryTextBox);
+            this.Name = "CustEdit";
             this.Text = "CustEdit";
+            this.Load += new System.EventHandler(this.CustEdit_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dabAgencyDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customerTBBS)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private DabAgencyDataSet dabAgencyDataSet;
+        private System.Windows.Forms.BindingSource customerTBBS;
+        private DabAgencyDataSetTableAdapters.CustomerTBTableAdapter customerTA;
+        private DabAgencyDataSetTableAdapters.TableAdapterManager tam;
+        private System.Windows.Forms.TextBox nRICTextBox;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox contactTextBox;
+        private System.Windows.Forms.TextBox addressTextBox;
+        private System.Windows.Forms.DateTimePicker date_of_BirthDateTimePicker;
+        private System.Windows.Forms.TextBox countryTextBox;
+        private System.Windows.Forms.Button btnSave;
     }
 }
--- DabAgent/CustEdit.cs
+++ DabAgent/CustEdit.cs
@@ -16,5 +16,36 @@ namespace DabAgent
         {
             InitializeComponent();
         }
+
+      
+
+        private void CustEdit_Load(object sender, EventArgs e)
+        {
+            customerTA.Fill(dabAgencyDataSet.CustomerTB);
+            
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            //Customer custclass = new Customer();
+            //custclass.NRIC = nRICTextBox.Text;
+            
+            //bool a = custclass.IsNRICValid();
+            //if (a.Equals(true))
+            //{
+            //    this.customerTBBS.EndEdit();
+            //    this.tam.UpdateAll(this.dabAgencyDataSet);
+            //    this.Close();
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Invalid IC");
+            //}
+            //FIX THIS IDK HOW TO(KEEGAN)
+            
+        
+           
+        }
     }
 }
