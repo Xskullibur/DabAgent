2732158cc4275f5071401e6ad68bcc7e570f01c2
Alson <alson_sim_wei_jie@connect.ite.edu.sg>
2018-02-06 01:03:55 +0800

    +Add Booking Design

--- DabAgent/AddBooking.Designer.cs
+++ DabAgent/AddBooking.Designer.cs
@@ -38,21 +38,19 @@
             System.Windows.Forms.Label bookingDateLabel;
             this.TabControl = new System.Windows.Forms.TabControl();
             this.PersonalTab = new System.Windows.Forms.TabPage();
-            this.nRICTextBox = new System.Windows.Forms.TextBox();
+            this.bookingDateComboBox = new System.Windows.Forms.ComboBox();
             this.bsBooking = new System.Windows.Forms.BindingSource(this.components);
             this.ds = new DabAgent.DabAgencyDataSet();
+            this.nRICTextBox = new System.Windows.Forms.TextBox();
             this.travelDateDateTimePicker = new System.Windows.Forms.DateTimePicker();
             this.paxTextBox = new System.Windows.Forms.TextBox();
             this.FlightTab = new System.Windows.Forms.TabPage();
             this.arrivalComboBox = new System.Windows.Forms.ComboBox();
             this.departureComboBox = new System.Windows.Forms.ComboBox();
-            this.CalBtn = new System.Windows.Forms.Button();
-            this.DistLbl = new System.Windows.Forms.Label();
             this.costPxTextBox = new System.Windows.Forms.TextBox();
             this.SaveBtn = new System.Windows.Forms.Button();
             this.taBooking = new DabAgent.DabAgencyDataSetTableAdapters.BookingTBTableAdapter();
             this.tam = new DabAgent.DabAgencyDataSetTableAdapters.TableAdapterManager();
-            this.bookingDateComboBox = new System.Windows.Forms.ComboBox();
             nRICLabel = new System.Windows.Forms.Label();
             travelDateLabel = new System.Windows.Forms.Label();
             paxLabel = new System.Windows.Forms.Label();
@@ -70,7 +68,7 @@
             // nRICLabel
             // 
             nRICLabel.AutoSize = true;
-            nRICLabel.Location = new System.Drawing.Point(21, 23);
+            nRICLabel.Location = new System.Drawing.Point(36, 32);
             nRICLabel.Name = "nRICLabel";
             nRICLabel.Size = new System.Drawing.Size(52, 20);
             nRICLabel.TabIndex = 4;
@@ -79,7 +77,7 @@
             // travelDateLabel
             // 
             travelDateLabel.AutoSize = true;
-            travelDateLabel.Location = new System.Drawing.Point(21, 56);
+            travelDateLabel.Location = new System.Drawing.Point(36, 82);
             travelDateLabel.Name = "travelDateLabel";
             travelDateLabel.Size = new System.Drawing.Size(94, 20);
             travelDateLabel.TabIndex = 6;
@@ -88,7 +86,7 @@
             // paxLabel
             // 
             paxLabel.AutoSize = true;
-            paxLabel.Location = new System.Drawing.Point(21, 121);
+            paxLabel.Location = new System.Drawing.Point(36, 178);
             paxLabel.Name = "paxLabel";
             paxLabel.Size = new System.Drawing.Size(39, 20);
             paxLabel.TabIndex = 8;
@@ -97,7 +95,7 @@
             // costPxLabel
             // 
             costPxLabel.AutoSize = true;
-            costPxLabel.Location = new System.Drawing.Point(18, 84);
+            costPxLabel.Location = new System.Drawing.Point(46, 151);
             costPxLabel.Name = "costPxLabel";
             costPxLabel.Size = new System.Drawing.Size(67, 20);
             costPxLabel.TabIndex = 16;
@@ -106,7 +104,7 @@
             // departureLabel
             // 
             departureLabel.AutoSize = true;
-            departureLabel.Location = new System.Drawing.Point(18, 16);
+            departureLabel.Location = new System.Drawing.Point(46, 53);
             departureLabel.Name = "departureLabel";
             departureLabel.Size = new System.Drawing.Size(85, 20);
             departureLabel.TabIndex = 25;
@@ -115,11 +113,20 @@
             // arrivalLabel
             // 
             arrivalLabel.AutoSize = true;
-            arrivalLabel.Location = new System.Drawing.Point(18, 50);
+            arrivalLabel.Location = new System.Drawing.Point(46, 102);
             arrivalLabel.Name = "arrivalLabel";
             arrivalLabel.Size = new System.Drawing.Size(56, 20);
             arrivalLabel.TabIndex = 26;
             arrivalLabel.Text = "Arrival:";
+            // 
+            // bookingDateLabel
+            // 
+            bookingDateLabel.AutoSize = true;
+            bookingDateLabel.Location = new System.Drawing.Point(36, 128);
+            bookingDateLabel.Name = "bookingDateLabel";
+            bookingDateLabel.Size = new System.Drawing.Size(110, 20);
+            bookingDateLabel.TabIndex = 9;
+            bookingDateLabel.Text = "Booking Date:";
             // 
             // TabControl
             // 
@@ -128,7 +135,7 @@
             this.TabControl.Location = new System.Drawing.Point(12, 12);
             this.TabControl.Name = "TabControl";
             this.TabControl.SelectedIndex = 0;
-            this.TabControl.Size = new System.Drawing.Size(404, 237);
+            this.TabControl.Size = new System.Drawing.Size(534, 276);
             this.TabControl.TabIndex = 0;
             // 
             // PersonalTab
@@ -145,18 +152,20 @@
             this.PersonalTab.Location = new System.Drawing.Point(4, 29);
             this.PersonalTab.Name = "PersonalTab";
             this.PersonalTab.Padding = new System.Windows.Forms.Padding(3);
-            this.PersonalTab.Size = new System.Drawing.Size(396, 204);
+            this.PersonalTab.Size = new System.Drawing.Size(526, 243);
             this.PersonalTab.TabIndex = 0;
             this.PersonalTab.Text = "Personal Details";
             this.PersonalTab.UseVisualStyleBackColor = true;
             // 
-            // nRICTextBox
+            // bookingDateComboBox
             // 
-            this.nRICTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.bsBooking, "NRIC", true));
-            this.nRICTextBox.Location = new System.Drawing.Point(137, 20);
-            this.nRICTextBox.Name = "nRICTextBox";
-            this.nRICTextBox.Size = new System.Drawing.Size(247, 26);
-            this.nRICTextBox.TabIndex = 5;
+            this.bookingDateComboBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.bsBooking, "BookingDate", true));
+            this.bookingDateComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.bookingDateComboBox.FormattingEnabled = true;
+            this.bookingDateComboBox.Location = new System.Drawing.Point(152, 125);
+            this.bookingDateComboBox.Name = "bookingDateComboBox";
+            this.bookingDateComboBox.Size = new System.Drawing.Size(247, 28);
+            this.bookingDateComboBox.TabIndex = 10;
             // 
             // bsBooking
             // 
@@ -168,10 +177,18 @@
             this.ds.DataSetName = "DabAgencyDataSet";
             this.ds.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
             // 
+            // nRICTextBox
+            // 
+            this.nRICTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.bsBooking, "NRIC", true));
+            this.nRICTextBox.Location = new System.Drawing.Point(152, 29);
+            this.nRICTextBox.Name = "nRICTextBox";
+            this.nRICTextBox.Size = new System.Drawing.Size(247, 26);
+            this.nRICTextBox.TabIndex = 5;
+            // 
             // travelDateDateTimePicker
             // 
             this.travelDateDateTimePicker.DataBindings.Add(new System.Windows.Forms.Binding("Value", this.bsBooking, "TravelDate", true));
-            this.travelDateDateTimePicker.Location = new System.Drawing.Point(137, 52);
+            this.travelDateDateTimePicker.Location = new System.Drawing.Point(152, 77);
             this.travelDateDateTimePicker.Name = "travelDateDateTimePicker";
             this.travelDateDateTimePicker.Size = new System.Drawing.Size(247, 26);
             this.travelDateDateTimePicker.TabIndex = 7;
@@ -180,7 +197,7 @@
             // paxTextBox
             // 
             this.paxTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.bsBooking, "Pax", true));
-            this.paxTextBox.Location = new System.Drawing.Point(137, 118);
+            this.paxTextBox.Location = new System.Drawing.Point(152, 175);
             this.paxTextBox.Name = "paxTextBox";
             this.paxTextBox.Size = new System.Drawing.Size(247, 26);
             this.paxTextBox.TabIndex = 9;
@@ -191,14 +208,12 @@
             this.FlightTab.Controls.Add(this.arrivalComboBox);
             this.FlightTab.Controls.Add(departureLabel);
             this.FlightTab.Controls.Add(this.departureComboBox);
-            this.FlightTab.Controls.Add(this.CalBtn);
-            this.FlightTab.Controls.Add(this.DistLbl);
             this.FlightTab.Controls.Add(costPxLabel);
             this.FlightTab.Controls.Add(this.costPxTextBox);
             this.FlightTab.Location = new System.Drawing.Point(4, 29);
             this.FlightTab.Name = "FlightTab";
             this.FlightTab.Padding = new System.Windows.Forms.Padding(3);
-            this.FlightTab.Size = new System.Drawing.Size(396, 204);
+            this.FlightTab.Size = new System.Drawing.Size(526, 243);
             this.FlightTab.TabIndex = 1;
             this.FlightTab.Text = "Flight Details";
             this.FlightTab.UseVisualStyleBackColor = true;
@@ -231,7 +246,7 @@
             "Japan",
             "Canada",
             "Morocco"});
-            this.arrivalComboBox.Location = new System.Drawing.Point(134, 47);
+            this.arrivalComboBox.Location = new System.Drawing.Point(162, 99);
             this.arrivalComboBox.Name = "arrivalComboBox";
             this.arrivalComboBox.Size = new System.Drawing.Size(199, 28);
             this.arrivalComboBox.TabIndex = 27;
@@ -264,42 +279,22 @@
             "Japan",
             "Canada",
             "Morocco"});
-            this.departureComboBox.Location = new System.Drawing.Point(134, 13);
+            this.departureComboBox.Location = new System.Drawing.Point(162, 50);
             this.departureComboBox.Name = "departureComboBox";
             this.departureComboBox.Size = new System.Drawing.Size(199, 28);
             this.departureComboBox.TabIndex = 26;
             // 
-            // CalBtn
-            // 
-            this.CalBtn.Location = new System.Drawing.Point(207, 145);
-            this.CalBtn.Name = "CalBtn";
-            this.CalBtn.Size = new System.Drawing.Size(126, 30);
-            this.CalBtn.TabIndex = 25;
-            this.CalBtn.Text = "Calculate Cost";
-            this.CalBtn.UseVisualStyleBackColor = true;
-            this.CalBtn.Visible = false;
-            // 
-            // DistLbl
-            // 
-            this.DistLbl.AutoSize = true;
-            this.DistLbl.Location = new System.Drawing.Point(130, 122);
-            this.DistLbl.Name = "DistLbl";
-            this.DistLbl.Size = new System.Drawing.Size(116, 20);
-            this.DistLbl.TabIndex = 23;
-            this.DistLbl.Text = "Distance: 0000";
-            this.DistLbl.Visible = false;
-            // 
             // costPxTextBox
             // 
             this.costPxTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.bsBooking, "CostPx", true));
-            this.costPxTextBox.Location = new System.Drawing.Point(134, 81);
+            this.costPxTextBox.Location = new System.Drawing.Point(162, 148);
             this.costPxTextBox.Name = "costPxTextBox";
             this.costPxTextBox.Size = new System.Drawing.Size(200, 26);
             this.costPxTextBox.TabIndex = 17;
             // 
             // SaveBtn
             // 
-            this.SaveBtn.Location = new System.Drawing.Point(308, 255);
+            this.SaveBtn.Location = new System.Drawing.Point(442, 294);
             this.SaveBtn.Name = "SaveBtn";
             this.SaveBtn.Size = new System.Drawing.Size(104, 42);
             this.SaveBtn.TabIndex = 1;
@@ -319,33 +314,19 @@
             this.tam.HolidayTBTableAdapter = null;
             this.tam.UpdateOrder = DabAgent.DabAgencyDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
             // 
-            // bookingDateLabel
-            // 
-            bookingDateLabel.AutoSize = true;
-            bookingDateLabel.Location = new System.Drawing.Point(21, 87);
-            bookingDateLabel.Name = "bookingDateLabel";
-            bookingDateLabel.Size = new System.Drawing.Size(110, 20);
-            bookingDateLabel.TabIndex = 9;
-            bookingDateLabel.Text = "Booking Date:";
-            // 
-            // bookingDateComboBox
-            // 
-            this.bookingDateComboBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.bsBooking, "BookingDate", true));
-            this.bookingDateComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.bookingDateComboBox.FormattingEnabled = true;
-            this.bookingDateComboBox.Location = new System.Drawing.Point(137, 84);
-            this.bookingDateComboBox.Name = "bookingDateComboBox";
-            this.bookingDateComboBox.Size = new System.Drawing.Size(247, 28);
-            this.bookingDateComboBox.TabIndex = 10;
-            // 
             // AddBooking
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(428, 305);
+            this.BackgroundImage = global::DabAgent.Properties.Resources.sky;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(558, 348);
             this.Controls.Add(this.SaveBtn);
             this.Controls.Add(this.TabControl);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "AddBooking";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "AddBooking";
             this.Load += new System.EventHandler(this.AddBooking_Load);
             this.TabControl.ResumeLayout(false);
@@ -373,8 +354,6 @@
         private System.Windows.Forms.TextBox paxTextBox;
         private System.Windows.Forms.TextBox costPxTextBox;
         private System.Windows.Forms.Button SaveBtn;
-        private System.Windows.Forms.Label DistLbl;
-        private System.Windows.Forms.Button CalBtn;
         private System.Windows.Forms.ComboBox arrivalComboBox;
         private System.Windows.Forms.ComboBox departureComboBox;
         private System.Windows.Forms.ComboBox bookingDateComboBox;
--- DabAgent/Properties/Resources.Designer.cs
+++ DabAgent/Properties/Resources.Designer.cs
@@ -119,5 +119,15 @@ namespace DabAgent.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap sky {
+            get {
+                object obj = ResourceManager.GetObject("sky", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
